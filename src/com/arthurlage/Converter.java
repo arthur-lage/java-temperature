02b4a78e5a3d17474b12e3c78318a693d2793c8f
Arthur Lage <arthurlage2006@gmail.com>
2022-08-08 18:19:05 -0300

    Add new functionalities, such as: Convert from fahrenheit to kelvin/celsius, celsius to kelvin/fahrenheit, kelvin to fahrenheit/celsius

--- src/com/arthurlage/Converter.java
+++ src/com/arthurlage/Converter.java
@@ -1,15 +1,35 @@
 package com.arthurlage;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.lang.Math;
 
 public class Converter {
-    public static long convertToFahrenheit(double temperatureInCelsius) {
-        return Math.round((temperatureInCelsius * 9/5) + 32);
+    public static long convertToFahrenheit(double temperature, String from) {
+        if(from.equals("kelvin")) {
+            return Math.round((temperature - 273.15) * 9/5 + 32);
+        } else if (from.equals("celsius")) {
+            return Math.round((temperature * 9 / 5) + 32);
+        } else {
+            return 0;
+        }
     }
 
-    public static long convertToCelsius(double temperatureInFahrenheit) {
-        return Math.round((temperatureInFahrenheit - 32) * 5/9);
+    public static long convertToCelsius(double temperature, String from) {
+        if(from.equals("fahrenheit")) {
+            return Math.round((temperature - 32) * 5/9);
+        } else if (from.equals("kelvin")) {
+            return Math.round(temperature - 273.15);
+        } else {
+            return 0;
+        }
+    }
+
+    public static long convertToKelvin(double temperature, String from) {
+        if(from.equals("fahrenheit")) {
+            return Math.round((temperature - 32) * 5/9 + 273.15);
+        } else if (from.equals("celsius")) {
+            return Math.round(temperature + 273.15);
+        } else {
+            return 0;
+        }
     }
 }
--- src/com/arthurlage/Main.java
+++ src/com/arthurlage/Main.java
@@ -7,27 +7,67 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Converter converter = new Converter();
 
-        System.out.println("Convert to: \n" +
-                "1 - Fahrenheit\n" +
-                "2 - Celsius\n");
+        System.out.println("""
+                Convert to:\s
+                1 - Fahrenheit to Celsius
+                2 - Fahrenheit to Kelvin
+                3 - Celsius to Fahrenheit
+                4 - Celsius to Kelvin
+                5 - Kelvin to Fahrenheit
+                6 - Kelvin to Celsius
+                7 - Exit
+                """);
 
         int convertOption = scanner.nextInt();
 
         if(convertOption == 1) {
             System.out.println();
-            System.out.print("Type temperature in Celsius: ");
+            System.out.print("Type temperature in Fahrenheit: ");
 
-            double temperatureInC = scanner.nextDouble();
-            long temperatureInF = converter.convertToFahrenheit(temperatureInC);
-            System.out.println(Math.round(temperatureInC) + "C in Fahrenheit is: " + temperatureInF + "F");
+            double temperatureInF = scanner.nextDouble();
+            long temperatureInC = converter.convertToCelsius(temperatureInF, "fahrenheit");
+            System.out.println(Math.round(temperatureInF) + "F in Celsius is: " + temperatureInC + "C");
         } else if (convertOption == 2) {
             System.out.println();
             System.out.print("Type temperature in Fahrenheit: ");
 
             double temperatureInF = scanner.nextDouble();
-            long temperatureInC = converter.convertToCelsius(temperatureInF);
-            System.out.println(Math.round(temperatureInF) + "F in Celsius is: " + temperatureInC + "C");
-        }
+            long temperatureInK = converter.convertToKelvin(temperatureInF, "fahrenheit");
+            System.out.println(Math.round(temperatureInF) + "F in Kelvin is: " + temperatureInK + "K");
+        } else if (convertOption == 3) {
+            System.out.println();
+            System.out.print("Type temperature in Celsius: ");
 
+            double temperatureInC = scanner.nextDouble();
+            long temperatureInF = converter.convertToFahrenheit(temperatureInC, "celsius");
+            System.out.println(Math.round(temperatureInC) + "C in Fahrenheit is: " + temperatureInF + "F");
+        }
+        else if (convertOption == 4) {
+            System.out.println();
+            System.out.print("Type temperature in Celsius: ");
+
+            double temperatureInC = scanner.nextDouble();
+            long temperatureInK = converter.convertToKelvin(temperatureInC, "celsius");
+            System.out.println(Math.round(temperatureInC) + "C in Kelvin is: " + temperatureInK + "K");
+        }
+        else if (convertOption == 5) {
+            System.out.println();
+            System.out.print("Type temperature in Kelvin: ");
+
+            double temperatureInK = scanner.nextDouble();
+            long temperatureInF = converter.convertToFahrenheit(temperatureInK, "kelvin");
+            System.out.println(Math.round(temperatureInK) + "K in Fahrenheit is: " + temperatureInF + "F");
+        }
+        else if (convertOption == 6) {
+            System.out.println();
+            System.out.print("Type temperature in Kelvin: ");
+
+            double temperatureInK = scanner.nextDouble();
+            long temperatureInC = converter.convertToCelsius(temperatureInK, "kelvin");
+            System.out.println(Math.round(temperatureInK) + "K in Celsius is: " + temperatureInC + "C");
+        } else if (convertOption == 7) {
+            System.out.println("Exiting...");
+            return;
+        }
     }
 }
